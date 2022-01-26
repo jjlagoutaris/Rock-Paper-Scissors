0d9c206b185d7108140696123e088cb23b2c3c5b
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-26 14:57:55 -0500

    I have made a bunch of changes so far, including adding buttons for the rps choices which allow the user to select one of those instead of typing out the choice as well as adding a div containing the score which updates with the outcomes of each round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,6 +8,59 @@
 
 let playerScore = 0, compScore = 0;
 
+const body = document.querySelector('body');
+body.style.cssText = 'height: 100%; background-color: #4f4f4f';
+const buttonsDiv = document.createElement('div');
+const rockBtn = document.createElement('button');
+const scissorsBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+
+buttonsDiv.className = 'buttonsDiv';
+rockBtn.className = 'button', paperBtn.className = 'button', scissorsBtn.className = 'button';
+
+buttonsDiv.style.cssText = 'display: flex; flew-grow: 1; justify-content: center; \
+align-items: center';
+rockBtn.style.cssText = 'width: 200px; height: 50px; font-size: 24px; font-family: Impact; \
+border: 2px solid black; border-radius: 15px; margin: 10px';
+paperBtn.style.cssText = 'width: 200px; height: 50px; font-size: 24px; font-family: Impact; \
+border: 2px solid black; border-radius: 15px; margin: 10px';
+scissorsBtn.style.cssText = 'width: 200px; height: 50px; font-size: 24px; font-family: Impact; \
+border: 2px solid black; border-radius: 15px; margin: 10px';
+
+rockBtn.textContent = 'Rock';
+scissorsBtn.textContent = 'Scissors';
+paperBtn.textContent = 'Paper';
+
+buttonsDiv.appendChild(rockBtn);
+buttonsDiv.appendChild(paperBtn);
+buttonsDiv.appendChild(scissorsBtn);
+body.appendChild(buttonsDiv);
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('click', buttonClicked));
+
+const scoreboard = document.createElement('div');
+scoreboard.className = 'scoreboard';
+scoreboard.style.cssText = 'display: flex; width: 100%; justify-content: space-evenly; \
+font-size: 24px; font-family: helvetica; margin-top: 50px';
+const playerScoreLabel = document.createElement('div');
+playerScoreLabel.className = 'score';
+playerScoreLabel.style.cssText = 'background-color: #6C63FF; width: 150px; text-align: center; \
+padding: 10px';
+const computerScoreLabel = document.createElement('div');
+computerScoreLabel.className = 'score';
+computerScoreLabel.style.cssText = 'background-color: #00BFA6; width: 150px; text-align: center; \
+padding: 10px'
+
+playerScoreLabel.textContent = "Player: " + playerScore;
+computerScoreLabel.textContent = "Computer: " + compScore; 
+
+scoreboard.appendChild(playerScoreLabel);
+scoreboard.appendChild(computerScoreLabel);
+body.appendChild(scoreboard);
+
+
+
 function computerPlay(){
     let number = Math.floor(Math.random() * 3) + 1;
     switch(number){
@@ -23,15 +76,8 @@ function computerPlay(){
     }
 }
 
-function playerSelection(){
-    input = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
-    if (input === 'rock' || input === 'paper' || input === 'scissors'){
-        return input;
-    }
-    else{
-        console.log('Error in playerSelection()');
-        return 'Error in playerSelection()';
-    }
+function buttonClicked(e){
+    round((e.target.textContent.toLowerCase()), computerPlay());
 }
 
 function round(playerMove, computerMove){
@@ -69,17 +115,20 @@ function round(playerMove, computerMove){
 }
 
 function gameLoop(){
+
+    round(playerSelection(), computerPlay());
+    console.log(`Player: ${playerScore} Computer: ${compScore}`)
     
-    while (playerScore < 5 && compScore < 5){
-        round(playerSelection(), computerPlay());
-        console.log(`Player: ${playerScore} Computer: ${compScore}`)
-    }
-    if (playerScore === 5){
-        console.log("Congratulations - you win!");
-    }
-    else{
-        console.log("Hold this L.");
-    }
+    // while (playerScore < 5 && compScore < 5){
+    //     round(playerSelection(), computerPlay());
+    //     console.log(`Player: ${playerScore} Computer: ${compScore}`)
+    // }
+    // if (playerScore === 5){
+    //     console.log("Congratulations - you win!");
+    // }
+    // else{
+    //     console.log("Hold this L.");
+    // }
 }
 
-gameLoop();
+// gameLoop();
